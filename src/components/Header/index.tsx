a91d1d067170340c007a688872e88a212e98691c
lucaslopes <lucas.lopesx1@gmail.com>
2023-02-11 13:13:03 -0300

    project init

---
+++ src/components/Header/index.tsx
@@ -0,0 +1,10 @@
+import { HeaderContainer } from './styles'
+
+export function Header() {
+  return (
+    <HeaderContainer>
+      <div>Logo</div>
+      <div>Loc + Cart</div>
+    </HeaderContainer>
+  )
+}
---
+++ src/components/Header/styles.ts
@@ -0,0 +1,6 @@
+import styled from 'styled-components'
+
+export const HeaderContainer = styled.header`
+  background: gray;
+  height: 2rem;
+`
