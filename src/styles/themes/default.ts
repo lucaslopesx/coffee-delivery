cd9575a57ca41e3b09a255a148d5fe5e5a4b894f
lucaslopes <lucas.lopesx1@gmail.com>
2023-02-14 22:30:58 -0300

    refactor: better color variables

--- src/styles/themes/default.ts
+++ src/styles/themes/default.ts
@@ -1,15 +1,21 @@
 export const defaultTheme = {
-  'white-100': '#FAFAFA',
+  white: '#FFF',
 
-  'gray-400': '#574F4D',
-  'gray-600': '#403937',
-  'gray-700': '#272221',
+  background: '#FAFAFA',
+  card: '#F3F2F2',
+  input: '#EDEDED',
+  button: '#E6E5E5',
+  hover: '#D7D5D5',
+  label: '#8D8686',
+  text: '#574F4D',
+  subtitle: '#403937',
+  title: '#272221',
 
-  'yellow-300': '#F1E9C9',
-  'yellow-500': '#DBAC2C',
-  'yellow-800': '#C47F17',
+  'yellow-light': '#F1E9C9',
+  yellow: '#DBAC2C',
+  'yellow-dark': '#C47F17',
 
-  'purple-300': '#EBE5F9',
-  'purple-800': '#4B2995',
-  'purple-500': '#8047F8',
+  'purple-light': '#EBE5F9',
+  'purple-dark': '#4B2995',
+  purple: '#8047F8',
 }
